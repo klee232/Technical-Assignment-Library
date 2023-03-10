4d89320cd8f80d8817a1998c32bb346c36d5cc1a
Lee <klee232@ad.brown.edu>
2023-03-10 13:20:34 -0500

    make some modification regarding gabor filter function

--- Image_Segmentation/freq_wave_converter.m
+++ Image_Segmentation/freq_wave_converter.m
@@ -6,7 +6,16 @@
 % wavelength for usage for gabor filter function for MATLAB
 % Function Arguments:
 % freq: computed frequency from img_spatio_freq.m function
-function freq_wave_converter(freq)
+function wavelength=freq_wave_converter(freq)
+    % check out if the input is empty
+    % insert input parser for organizing the required and optional input
+    p=inputParser;
+    checkinput = @(x) ~isempty(freq);
+    addRequired(p,"freq",checkinput);
+    parse(p,freq);
+    freq=p.Results.freq;
 
-
+    % conduct frequency to wavelength conversion
+    wavelength=1/freq;
+    
 end
