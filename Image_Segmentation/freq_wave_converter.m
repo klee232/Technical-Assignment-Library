c197ed7d10004b6f0a275a0540d125896c91cc16
Lee <klee232@ad.brown.edu>
2023-03-09 20:53:03 -0500

    created frequency to wavelength convert

---
+++ Image_Segmentation/freq_wave_converter.m
@@ -0,0 +1,12 @@
+% Created Date: March 9th, 2023
+% Belong to: Leelab.ai
+% Creator: Kuan-Min Lee
+% Function Instruction:
+% This function is created to compute a conversion for frequency to
+% wavelength for usage for gabor filter function for MATLAB
+% Function Arguments:
+% freq: computed frequency from img_spatio_freq.m function
+function freq_wave_converter(freq)
+
+
+end
